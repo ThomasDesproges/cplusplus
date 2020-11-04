683fba4da12721295684c92f7de8f7809d3f5bc6
unknown <thomas.desproges@mines-paristech.fr>
2020-11-04 16:53:28 +0100

    nouvelle version pile + test

--- rpn_calc/int_stack/pile.h
+++ rpn_calc/int_stack/pile.h
@@ -4,30 +4,49 @@ class IntStack{
 
 public:
 
-IntStack(int m,int v[]) : m(m),value(v){
+IntStack(int m,int t,int v[]) : m(m),top(t),value(v){
 }
 
 void push(int i){
     if (*p_top < m){
+    pile_remplie = false;
     value[*p_top] = i;
     *p_top = *p_top + 1;
     return;
     }
+    if (pile_remplie == false){
     std::cout << "Pile Remplie" << std::endl;
-
+    }
+    pile_remplie = true;
+    return;
 }
 
 int pop(){
     int sortie = value[*p_top];
-    *p_top --;
+    *p_top = *p_top - 1;
     return sortie;
 }
 
+bool is_full(){
+    if (top < m){
+        return false;
+    }
+    return true;
+}
+
+bool is_empty(){
+    if (top == 0){
+        return true;
+    }
+    return false;
+}
+
 void affiche(){
+    std::cout << "[ "; 
     for(int i =0;i<*p_top;i++){
         std::cout << value[i] << " ";
     }
-    std::cout << std::endl;
+    std::cout << "[" << std::endl;
     return;
 }
 
@@ -36,4 +55,5 @@ private:
     int* value;
     int top = 0;
     int* p_top = &top;
+    bool pile_remplie = false;
 };
--- rpn_calc/int_stack/test_pile.cpp
+++ rpn_calc/int_stack/test_pile.cpp
@@ -4,12 +4,19 @@
 
 int main(){
     int taille = 30;
+    int top = 4;
     int st[taille] = {9,7,4,3};
-    IntStack stack(taille,st);
+    IntStack stack(20,top,st);
     stack.push(4);
     stack.push(8);
     for(int i =0;i<40;i++){
-        stack.push(i);
+        stack.push(-i);
     }
+    stack.affiche();
+    stack.pop();
+    stack.push(10);
+    stack.push(10);
+    std::cout << stack.is_full() << std::endl;
+    stack.is_full();
     stack.affiche();
 }
